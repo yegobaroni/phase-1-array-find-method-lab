2ae6738436ec7126123c172411ac4a77daa703d3
yego <yegokipkemoi254@gmail.com>
2022-08-29 07:08:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+function superbowlWin(array){
+
+    const win = array.find(({result}) => result === "W");
+    
+    if (win){
+    
+    return win.year;
+    
+    }
+    
+    else{
+    
+    return undefined
+    
+    }
+    
+    }
